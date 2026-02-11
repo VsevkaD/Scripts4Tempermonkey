977e4566005da477b6e43d76cb7b82cdd4785edd
VsevkaD <64196025+VsevkaD@users.noreply.github.com>
2026-02-11 23:40:30 +0300

    Update ReYo-CX.user.js

--- ReYo-CX.user.js
+++ ReYo-CX.user.js
@@ -4,7 +4,7 @@
 // @version        1.0.0
 // @description    Вставка плеера из CX в ReYo
 
-// @author         vsevkad1
+// @author         vsevkad
 // @author         нейрослоп
 
 // @updateURL      https://raw.githubusercontent.com/VsevkaD/Scripts4Tempermonkey/main/ReYo-CX.user.js
